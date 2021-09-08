d175a4684087a41096629c9fab9f82ef15f6cee2
rafaelsagawe <sagawe_r@hotmail.com>
2021-09-07 21:14:02 -0300

    Aplicando recursos visuais, paginação, busca e documentação

--- Web/Models/Equipamentos.cs
+++ Web/Models/Equipamentos.cs
@@ -11,23 +11,29 @@ namespace Web.Models
         [Key]
         public int IdEquipamento { get; set; }
 
+        [Display(Name ="Descrição")]
         public string EquipDescricao { get; set; }
 
+        [Display(Name ="Numero de Serie")]
         public string EquipNuSerie { get; set; }
 
+        [Display(Name ="Numero de patrimonio")]
         public string EquipNuControle { get; set; }
 
         // Setor ou escola onde esta instalado
 
 
         // Tipo Impressora, notebook, computador
+        [Display(Name ="Tipo")]
         public string EquipTipo { get; set; }
 
         //Alugado ou proprio
         public string EquipOrigem { get; set; }
 
+        [Display(Name = "Valor do equipamento")]
         public int EquipValor { get; set; }
 
+        [Display(Name ="Unidade de locação")]
         public int IdUE { get; set; }
 
         //public string EquipLocalizacao { get; set; }
