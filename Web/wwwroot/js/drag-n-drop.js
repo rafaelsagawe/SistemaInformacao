8067d3432be161c835cd5e4490ea025129958cc6
Rafael Sagawe Favero <sagawe_r@hotmail.com>
2021-09-23 15:06:41 -0300

    Tentativa de criação de arrasta e soltar na página do portefolio

---
+++ Web/wwwroot/js/drag-n-drop.js
@@ -0,0 +1,14 @@
+﻿const dragStart = event => {
+    event.currentTarget.classList.add('dragging');
+};
+
+const dragEnd = event => {
+    event.currentTarget.classList.remove('dragging');
+};
+
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('dragstart', dragStart);
+    card.addEventListener('dragend', dragEnd);
+});
+
+https://www.webtips.dev/how-to-create-native-drag-and-drop-functionality-in-javascript
